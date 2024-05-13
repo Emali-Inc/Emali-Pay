31c62aa80962c546bc17357369712cc582a2c235
oyaro <hillaryoyaroh@gmail.com>
2024-05-13 22:32:38 +0300

    update the route page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,19 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = { firstName: 'Oyaro'};
+
   return (
     <section className="home">
         <div className="home-content">
             <header className="home-header">
-                Welcome,Oyaro
+                <HeaderBox
+                type="greeting"
+                title="Welcome"
+                user={loggedIn?.firstName || 'Guest'}
+                subtext="Acess and manage your account and transactions efficiently."
+                />
             </header>
         </div>
     </section>
