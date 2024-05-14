3aed2a9472a536ee3f5e36cda96dcfda1dc84cee
oyaro <hillaryoyaroh@gmail.com>
2024-05-14 12:42:19 +0300

    implement update sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,12 @@
+"use client";
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 const Sidebar = ({user}:SiderbarProps) => {
+  const pathname = usePathname();
   return (
     <section className="sidebar">
         <nav className="flex flex-col gap-4">
@@ -11,8 +16,31 @@ const Sidebar = ({user}:SiderbarProps) => {
                     width={34}
                     height={34}
                     alt="EmaliPay logo"
+                    className="size-[24px] max-xl:size-14"
                 />
+                <h1 className="sidebar-logo">EmaliPay</h1>
             </Link>
+            {sidebarLinks.map((item)=>{
+               const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
+
+              return(
+                <Link  
+                  className={cn('sidebar-link',{'bg-bank-gradient': isActive})} 
+                  href={item.route} 
+                  key={item.label}>
+                    <div className="relative size-6">
+                      <Image 
+                        src={item.imgURL} 
+                        alt={item.label} 
+                        fill
+                        className={cn({
+                          'brightness-[3] invert-0': isActive
+                        })}
+                      />
+                    </div>
+                </Link>
+              )
+            })}
         </nav>
     </section>
   )
