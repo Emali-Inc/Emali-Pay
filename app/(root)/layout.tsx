60482cf666e0aa9a37c42676b4aebddde937e1b4
oyaro <hillaryoyaroh@gmail.com>
2024-05-16 17:26:41 +0300

    implement the root-layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,10 @@
 import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+
 import Image from "next/image";
 
-//The Root Layout was created by Nextjs
-export default function RootLayout({
+
+export default  function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -11,17 +12,17 @@ export default function RootLayout({
   const loggedIn={firstName: 'Hillary',lastName: "Oyaro"}
   return (
     <main className="flex h-screen w-full font-inter">
-        <Sidebar user={loggedIn}/>
+      <Sidebar user={loggedIn} />
 
-        <div className="flex size-full flex-col">
-          <div className="root-layout">
-            <Image src="/icons/logo.svg" width={30} height={30} alt="logo"/>
-            <div>
-              <MobileNav user={loggedIn}/>
-            </div>
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
           </div>
         </div>
         {children}
+      </div>
     </main>
   );
 }
