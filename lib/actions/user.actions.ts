aa9c929b7d74298d4b2827703ca7276e8a874194
oyaro <hillaryoyaroh@gmail.com>
2024-05-20 19:14:32 +0300

    implement server-side action from lib actions by structure defination

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+'use server';
+
+export const SignIn = async () => {
+    try {
+       //Mutation /Modify the database  Make fetch
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const SignUn = async () => {
+    try {
+       //Mutation /Modify the database  Make fetch
+    } catch (error) {
+        console.error(error);
+    }
+}
