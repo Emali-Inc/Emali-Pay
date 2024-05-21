35a04b2c718b81e895f4e72c278e67b3995bf684
oyaro <hillaryoyaroh@gmail.com>
2024-05-21 12:06:23 +0300

    implementing the appwrite  signup and login sessions and cookies from appwrite SDKs and next-headers

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,18 +1,63 @@
 'use server';
 
-export const SignIn = async () => {
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
+
+export const SignIn = async ({ email,password }: signInProps) => {
     try {
        //Mutation /Modify the database  Make fetch
-    } catch (error) {
+       const { account } = await createAdminClient();
+
+       const response = await account.createEmailPasswordSession(email,password);
+       return parseStringify(response);
+    }  catch (error) {
         console.error(error);
     }
 }
 
 export const SignUp = async (userData: SignUpParams) => {
+    //Destructuring the user data syntax
+    const { email,password,firstName,lastName } = userData;
     try {
-       //Mutation /Modify the database  Make fetch
-       //Create user account
+       //Mutation /Modify the database  Make fetch /Create user account
+       const { account } = await createAdminClient();
+
+       const newUser = await account.create(
+        ID.unique(), 
+        email, 
+        password,
+         `${firstName} ${lastName}`
+        );
+        //creating the session by awing the email and password
+       const session = await account.createEmailPasswordSession(email, password);
+        
+       //import the cookies from next-headers library of nextjs
+       cookies().set("emali-appwrite-session", session.secret, {
+         path: "/",
+         httpOnly: true,
+         sameSite: "strict",
+         secure: true,
+       });
+       //in nextjs we cannot pass large object such as entire user object
+       //through server actions rather we have to stringify and call the function from utils.ts file 
+       return parseStringify(newUser);
+     
     } catch (error) {
         console.error(error);
     }
 }
+
+// ... your initilization functions
+export async function getLoggedInUser() {
+    try {
+      const { account } = await createSessionClient();
+      const user = await account.get();
+
+      return parseStringify(user);
+    } catch (error) {
+      return null;
+    }
+  }
+  
