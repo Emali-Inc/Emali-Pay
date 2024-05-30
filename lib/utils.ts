290a74b0834d39c4c514c4235abc665dc4005e15
oyaro <hillaryoyaroh@gmail.com>
2024-05-30 20:03:15 +0300

    update utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -87,7 +87,20 @@ export function formatAmountKenya(amount: number): string {
   return formatter.format(amount);
 }
 
-export const parseStringify = (value: any) => JSON.parse(JSON.stringify(value));
+export const parseStringify = (value: any) => {
+  if (value === undefined || value === null) {
+    console.error("parseStringify received an invalid value:", value);
+    return value;
+  }
+
+  try {
+    return JSON.parse(JSON.stringify(value));
+  } catch (error) {
+    console.error('Error parsing JSON:', error, 'Value:', value);
+    return null;
+  }
+};
+
 
 export const removeSpecialCharacters = (value: string) => {
   return value.replace(/[^\w\s]/gi, "");
@@ -207,17 +220,16 @@ export const getTransactionStatus = (date: Date) => {
 };
 
 export const authFormSchema = (type: string) => z.object({
-  //Sign Up
+  // sign up
   firstName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
   lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
   address1: type === 'sign-in' ? z.string().optional() : z.string().max(50),
   city: type === 'sign-in' ? z.string().optional() : z.string().max(50),
-  state: type === 'sign-in' ? z.string().optional() : z.string().min(2).max(8),
-  postalcode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
+  state: type === 'sign-in' ? z.string().optional() : z.string().min(2).max(50),
+  postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
   dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
   ssn: type === 'sign-in' ? z.string().optional() : z.string().min(3),
-
-  //Both SignIn SignUp
+  // both
   email: z.string().email(),
   password: z.string().min(8),
 })
