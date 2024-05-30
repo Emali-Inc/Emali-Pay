1f42f54d8a3ac99e9a2bf634e52ea8aafeaa94d3
oyaro <hillaryoyaroh@gmail.com>
2024-05-30 19:59:30 +0300

    update on Rightsidebar

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -4,7 +4,12 @@ import React from 'react'
 import BankCard from './BankCard'
 
 const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
- 
+  console.log('User object:', user);
+  console.log('bank user',banks)
+  if (!user) {
+    return <div>Loading...</div>; // Handle the case where user is null
+  }
+
 
   return (
     <aside className="right-sidebar">
@@ -12,12 +17,12 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
         <div className="profile-banner" />
         <div className="profile">
           <div className="profile-img">
-            <span className="text-5xl font-bold text-blue-500">{user.firstName[0]}</span>
+            <span className="text-5xl font-bold text-blue-500">{user.name[0]}</span>
           </div>
 
           <div className="profile-details">
             <h1 className='profile-name'>
-              {user.firstName} {user.lastName}
+              {user.name}
             </h1>
             <p className="profile-email">
               {user.email}
