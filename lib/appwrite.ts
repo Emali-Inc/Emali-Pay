e297b966f909d8229b3b8587380790eb7cf96142
oyaro <hillaryoyaroh@gmail.com>
2024-06-15 23:19:29 +0300

    implement SSR in appwrite.ts

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,55 +1,28 @@
 "use server";
 
-import { Account, Client, Databases, Users } from "node-appwrite";
+import { Client, Account, Databases, Users } from "node-appwrite";
 import { cookies } from "next/headers";
 
-
-
-// Initialize session client
 export async function createSessionClient() {
-  try {
-    // Create a new Appwrite client
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
-    
-    // Retrieve session token from cookie
-    const session = cookies().get("emali-appwrite-session");
-    console.log("Get Cookies:",session)
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
 
-    // Check if session token exists
-    if (!session || !session.value) {
-      throw new Error("No session found in cookies");
-    }
+  const session = cookies().get("appwrite-session");
 
-    // Extract session token value
-    const sessionToken = session.value;
-
-    // Validate the session token format (must be a JWT with 3 segments)
-    //if (!isValidSessionToken(sessionToken)) {
-     // throw new Error("Invalid session token format");
-   // }
-
-    // Set session token for the client
-    client.setSession(sessionToken);
-
-    // Return the session client with the associated account
-    return {
-      account: new Account(client),
-    };
-  } catch (error) {
-    console.error('Error creating session client:', error);
-    throw error; // Rethrow the error for handling in the calling function
+  if (!session || !session.value) {
+    throw new Error("No session");
   }
+
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+  };
 }
 
-
-// Function to validate session token format
-//function isValidSessionToken(token: string): boolean {
-  //return token.split('.').length === 3;
-//}
-
-// Initialize admin client
 export async function createAdminClient() {
   const client = new Client()
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
@@ -57,8 +30,14 @@ export async function createAdminClient() {
     .setKey(process.env.NEXT_APPWRITE_KEY!);
 
   return {
-    account: new Account(client),
-    database: new Databases(client),
-    users: new Users(client),
+    get account() {
+      return new Account(client);
+    },
+    get database() {
+      return new Databases(client);
+    },
+    get user() {
+      return new Users(client);
+    }
   };
 }
