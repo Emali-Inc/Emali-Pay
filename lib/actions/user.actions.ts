f1c21ce9f7d72a1185b76a362f5c7bb621ccf198
oyaro <hillaryoyaroh@gmail.com>
2024-06-16 23:14:09 +0300

    implement server action loogics

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,14 +3,20 @@
 import { ID, Query } from "node-appwrite";
 import { createAdminClient, createSessionClient } from "../appwrite";
 import { cookies } from "next/headers";
-import {  parseStringify } from "../utils";
+import { parseStringify } from "../utils";
 
 const {
   APPWRITE_DATABASE_ID: DATABASE_ID,
   APPWRITE_USER_COLLECTION_ID: USER_COLLECTION_ID,
-  APPWRITE_BANK_COLLECTION_ID: BANK_COLLECTION_ID,
 } = process.env;
 
+/**
+ * Retrieves user information based on the provided user ID.
+ * 
+ * @param {Object} param - Parameters for the function.
+ * @param {string} param.userId - The ID of the user to retrieve information for.
+ * @returns {Promise<Object>} - The user information.
+ */
 export const getUserInfo = async ({ userId }: getUserInfoProps) => {
   try {
     const { database } = await createAdminClient();
@@ -19,53 +25,44 @@ export const getUserInfo = async ({ userId }: getUserInfoProps) => {
       DATABASE_ID!,
       USER_COLLECTION_ID!,
       [Query.equal('userId', [userId])]
-    )
+    );
 
     return parseStringify(user.documents[0]);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
-export const signIn = async ({ email, password }: signInProps) => {
-  try {
-    const { account } = await createAdminClient();
-    const session = await account.createEmailPasswordSession(email, password);
 
-    cookies().set("appwrite-session", session.secret, {
-      path: "/",
-      httpOnly: true,
-      sameSite: "strict",
-      secure: true,
-    });
-
-    const user = await getUserInfo({ userId: session.userId }) 
-
-    return parseStringify(user);
-  } catch (error) {
-    console.error('Error', error);
-  }
-}
-
+/**
+ * Signs up a new user.
+ * 
+ * @param {Object} param - Parameters for the function.
+ * @param {string} param.password - The user's password.
+ * @param {Object} param.userData - Additional user data.
+ * @param {string} param.userData.email - The user's email.
+ * @param {string} param.userData.firstName - The user's first name.
+ * @param {string} param.userData.lastName - The user's last name.
+ * @returns {Promise<Object>} - The newly created user information.
+ */
 export const signUp = async ({ password, ...userData }: SignUpParams) => {
+  //Destructure sythax of userData
   const { email, firstName, lastName } = userData;
-  
+
   let newUserAccount;
 
   try {
     const { account, database } = await createAdminClient();
-
+    //create new user by passing to appwrite account
     newUserAccount = await account.create(
-      ID.unique(), 
-      email, 
-      password, 
+      ID.unique(),
+      email,
+      password,
       `${firstName} ${lastName}`
     );
 
-    if(!newUserAccount) throw new Error('Error creating user')
+    if (!newUserAccount) throw new Error('Error creating user');
 
-    
-  
     const newUser = await database.createDocument(
       DATABASE_ID!,
       USER_COLLECTION_ID!,
@@ -73,10 +70,39 @@ export const signUp = async ({ password, ...userData }: SignUpParams) => {
       {
         ...userData,
         userId: newUserAccount.$id,
-        
       }
-    )
+    );
+     //Create session by awaiting account and Calling creatEmailPasswordSession from appwrite.
+    const session = await account.createEmailPasswordSession(email, password);
 
+    //Get the cookies by importing from next-header and attatch it to session.
+    cookies().set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+    //stringify the object since we cannot pass large object to server action.
+    return parseStringify(newUser);
+  } catch (error) {
+    console.error('Error', error);
+  }
+}
+
+/**
+ * Signs in a user using their email and password.
+ * 
+ * @param {Object} param - Parameters for the function.
+ * @param {string} param.email - The user's email.
+ * @param {string} param.password - The user's password.
+ * @returns {Promise<Object>} - The signed-in user information.
+ */
+export const signIn = async ({ email, password }: signInProps) => {
+  try {
+    //extract account from acreateAdminClient
+    const { account } = await createAdminClient();
+    
+    //Create session email and password using predefine method.
     const session = await account.createEmailPasswordSession(email, password);
 
     cookies().set("appwrite-session", session.secret, {
@@ -86,26 +112,38 @@ export const signUp = async ({ password, ...userData }: SignUpParams) => {
       secure: true,
     });
 
-    return parseStringify(newUser);
+    const user = await getUserInfo({ userId: session.userId });
+
+    return parseStringify(user);
   } catch (error) {
     console.error('Error', error);
   }
 }
 
+/**
+ * Retrieves the currently logged-in user's information.
+ * 
+ * @returns {Promise<Object|null>} - The logged-in user information or null if not logged in.
+ */
 export async function getLoggedInUser() {
   try {
     const { account } = await createSessionClient();
     const result = await account.get();
 
-    const user = await getUserInfo({ userId: result.$id})
+    const user = await getUserInfo({ userId: result.$id });
 
     return parseStringify(user);
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return null;
   }
 }
 
+/**
+ * Logs out the currently logged-in user.
+ * 
+ * @returns {Promise<null>} - Returns null on successful logout or error.
+ */
 export const logoutAccount = async () => {
   try {
     const { account } = await createSessionClient();
