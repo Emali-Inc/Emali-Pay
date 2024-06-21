ce430c500391f6312ef8bc3688ea8949b7c3b7ee
oyaro <hillaryoyaroh@gmail.com>
2024-06-21 23:56:08 +0300

    update--SideBar Component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,54 +1,60 @@
-"use client";
+'use client'
+
 import { sidebarLinks } from '@/constants'
 import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import Footer from './Footer';
+import Footer from './Footer'
+import PlaidLink from './PlaidLink'
+import MpesaLink from './MpesaLink'
 
-const Sidebar = ({user}:SiderbarProps) => {
+const Sidebar = ({ user }: SiderbarProps) => {
   const pathname = usePathname();
+
   return (
     <section className="sidebar">
-        <nav className="flex flex-col gap-0">
-            <Link href='/' className="mb-12 cursor-pointer flex items-center gap-2">
+      <nav className="flex flex-col gap-0">
+        <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
+          <Image 
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="Horizon logo"
+            className="size-[24px] max-xl:size-14"
+          />
+          <h1 className="sidebar-logo">OuterRing</h1>
+        </Link>
+
+        {sidebarLinks.map((item) => {
+          const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
+
+          return (
+            <Link href={item.route} key={item.label}
+              className={cn('sidebar-link', { 'bg-bank-gradient': isActive })}
+            >
+              <div className="relative size-6">
                 <Image 
-                    src="/icons/logo.svg"
-                    width={34}
-                    height={34}
-                    alt="EmaliPay logo"
-                    className="size-[24px] max-xl:size-14"
+                  src={item.imgURL}
+                  alt={item.label}
+                  fill
+                  className={cn({
+                    'brightness-[3] invert-0': isActive
+                  })}
                 />
-                <h1 className="sidebar-logo">EmaliPay</h1>
+              </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {item.label}
+              </p>
             </Link>
-            {sidebarLinks.map((item)=>{
-               const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
+          )
+        })}
+        
+        <PlaidLink user={user} />
+        <MpesaLink user={user} />
+      </nav>
 
-              return(
-                <Link  
-                  className={cn('sidebar-link',{'bg-bank-gradient': isActive})} 
-                  href={item.route} 
-                  key={item.label}>
-                    <div className="relative size-6">
-                      <Image 
-                        src={item.imgURL} 
-                        alt={item.label} 
-                        fill
-                        className={cn({
-                          'brightness-[3] invert-0': isActive
-                        })}
-                      />
-                    </div>
-                    <p className={cn("sidebar-label",{"!text-white" :isActive})}>
-                      {item.label}
-                    </p>
-                </Link>
-              )
-            })}
-
-            USER
-        </nav>
-        <Footer user={user}/>
+      <Footer user={user} />
     </section>
   )
 }
