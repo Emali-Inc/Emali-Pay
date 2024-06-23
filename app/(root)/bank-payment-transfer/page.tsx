dbc9300134d8f6906ea48883de3487e2d5b959ba
oyaro <hillaryoyaroh@gmail.com>
2024-06-23 23:22:09 +0300

    final implementation and dependence update

--- app/(root)/bank-payment-transfer/page.tsx
+++ app/(root)/bank-payment-transfer/page.tsx
@@ -1,8 +1,31 @@
+import HeaderBox from '@/components/HeaderBox'
+import PaymentTransferForm from '@/components/PaymentTransferForm'
+import { getAccounts } from '@/lib/actions/bank.actions';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 import React from 'react'
 
-const BankTransfer = () => {
+const BankTransfer = async () => {
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ 
+    userId: loggedIn.$id 
+  })
+
+    //if not accounts exit from this function not return the home page
+  if(!accounts) return;
+  
+  const accountsData = accounts?.data;
+ 
   return (
-    <div>BankTransfer</div>
+    <section className="payment-transfer">
+      <HeaderBox
+        title="Emali Bank Payment Transfer."
+        subtext="Please provide any specific detail or notes related to payment transfer."
+      />
+
+      <section className="size-full pt-5">
+        <PaymentTransferForm accounts={accountsData}/>
+      </section>
+    </section>
   )
 }
 
